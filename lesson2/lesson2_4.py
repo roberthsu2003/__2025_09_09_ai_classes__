1c805607037ae974c33e81fe6d0fe548c6c46145
徐國堂 <roberthsu2003@gmail.com>
2025-09-11 15:59:25 +0800

    modify

--- lesson2/lesson2_4.py
+++ lesson2/lesson2_4.py
@@ -6,9 +6,16 @@ iris = load_iris()
 print(type(iris))
 #取得特徵與標籤 
 X = iris.data
-print("X")
-print(X)
+print("X的資料類型:", type(X))
+print("X的形狀:", X.shape)
+print("X的內容:\n", X)
+
+#請使用pandas套件將X轉換成DataFrame格式，並命名欄位名稱為iris.feature_names
+import pandas as pd
+df_X = pd.DataFrame(X, columns=iris.feature_names)
+print("轉換後的DataFrame:\n", df_X)
 
 y = iris.target
-print("y")
-print(y)
+print("y的資料類型:", type(y))
+print("y的形狀:", y.shape)
+print("y的內容:", y)
